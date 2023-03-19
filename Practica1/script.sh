bbc48f2af9d46a1424c6ab38e730095e9860eb83
leon <l.elliottfuller@gmail.com>
2023-03-19 15:50:15 +0100

    Añadido los ficheros de salida .txt de los programas

--- Practica1/script.sh
+++ Practica1/script.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-output_file=./data/ficheroSalidaBorradoOrdenado.txt
+output_file=./data/ficheroSalidaBorrado.txt
 seed=123456
 
 # Generate arguments from 1000 to 30000 with an increment of 1000
 args=$(seq 1000 1000 100000)
 
 # Execute the program once with all arguments
-./bin/borradoDuplicadosIterativoOrdenado "$output_file" "$seed" $args
+./bin/borradoDuplicadosIterativo "$output_file" "$seed" $args
 
